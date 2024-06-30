fb8dec3eb38fcf71d8f2b60dc25ae3339a87e45c
James Kariuki <karisjaymo99@gmail.com>
2024-06-30 06:59:11 -0700

    Task 0, Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,54 @@
+const redis = require('redis');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    this.client.on('error', (err) => {
+      console.error('Redis client error:', err);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(value);
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (err) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve();
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve();
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
