bd498ed9cfeb6467d4d2a2e23d5d91ec109920cc
James Kariuki <karisjaymo99@gmail.com>
2024-07-01 22:12:17 -0700

    Task2, Express API

---
+++ controllers/AppController.js
@@ -0,0 +1,31 @@
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db').default;
+
+const getStatus = async (req, res) => {
+  try {
+    const redisAlive = await redisClient.isAlive();
+    const dbAlive = await dbClient.isAlive();
+
+    res.status(200).json({ redis: redisAlive, db: dbAlive });
+  } catch (error) {
+    console.error('Error checking status:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+const getStats = async (req, res) => {
+  try {
+    const numUsers = await dbClient.nbUsers();
+    const numFiles = await dbClient.nbFiles();
+
+    res.status(200).json({ users: numUsers, files: numFiles });
+  } catch (error) {
+    console.error('Error fetching stats:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+module.exports = {
+  getStatus,
+  getStats,
+};
